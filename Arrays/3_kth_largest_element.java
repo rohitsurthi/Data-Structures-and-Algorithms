6477e77004d52f62de7ca01ff191a40a3023667d
Rohit Surthi <34400138+rohitsurthi@users.noreply.github.com>
2021-05-18 19:10:17 +0530

    Create 3_kth_largest_element.java

---
+++ Arrays/3_kth_largest_element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for(int i=0; i<nums.length; i++){
+            q.add(nums[i]);
+            if(q.size() > k){
+                q.poll();
+            }
+        }
+        return q.poll();
+    }
+}
